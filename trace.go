d9813b2f05a9afa8e819900c0c1103f639e60967
KittenzExe <kitkittenz123@gmail.com>
2026-03-11 03:13:35 +0800

    :books: docs: added comments for traces

--- trace.go
+++ trace.go
@@ -35,12 +35,30 @@ func buildTrace(ts traceTimestamps, ingest, outgoing string, t *Trace, workerID,
 }
 
 // Call functions to access trace data
-func (t *Trace) PrepTime() time.Duration        { return t.timeToRequestToUpstream }
-func (t *Trace) WriteTime() time.Duration       { return t.timeToResponseFromProxy }
+
+// PrepTime returns the time taken to prepare and send the request to the upstream server
+func (t *Trace) PrepTime() time.Duration { return t.timeToRequestToUpstream }
+
+// WriteTime returns the time taken to write the response back to the client
+func (t *Trace) WriteTime() time.Duration { return t.timeToResponseFromProxy }
+
+// UpstreamLatency returns the time taken by the upstream server to process the request and send a response
 func (t *Trace) UpstreamLatency() time.Duration { return t.timeToResponseFromUpstream }
-func (t *Trace) TotalDuration() time.Duration   { return t.timeToCompleteRequest }
-func (t *Trace) ProxyOverhead() time.Duration   { return t.proxyProcessingTime }
-func (t *Trace) IngestEndpoint() string         { return t.ingestEndpoint }
-func (t *Trace) OutgoingEndpoint() string       { return t.outgoingEndpoint }
-func (t *Trace) WorkerPID() int                 { return t.workerPID }
-func (t *Trace) WorkerID() int                  { return t.workerID }
+
+// TotalDuration returns the total time taken from receiving the request to sending the response back to the client
+func (t *Trace) TotalDuration() time.Duration { return t.timeToCompleteRequest }
+
+// ProxyOverhead returns the time spent in the proxy itself (preparation + writing response)
+func (t *Trace) ProxyOverhead() time.Duration { return t.proxyProcessingTime }
+
+// IngestEndpoint returns the original request path that was ingested by the proxy
+func (t *Trace) IngestEndpoint() string { return t.ingestEndpoint }
+
+// OutgoingEndpoint returns the full URL that the proxy sent the request to upstream
+func (t *Trace) OutgoingEndpoint() string { return t.outgoingEndpoint }
+
+// WorkerPID returns the PID of the worker process that handled the request (prefork mode)
+func (t *Trace) WorkerPID() int { return t.workerPID }
+
+// WorkerID returns the ID of the worker process that handled the request (prefork mode)
+func (t *Trace) WorkerID() int { return t.workerID }
